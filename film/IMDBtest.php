1a51c53304673a0879807013bccfcdf51638816b
Gheedor <barthjuju54@gmail.com>
2023-03-30 14:49:06 +0200

    IMDB film

--- film/IMDBtest.php
+++ film/IMDBtest.php
@@ -1,19 +1,58 @@
+<style>
+  body{
+    background-color: #333;
+  }
+
+  div{
+    background-color: white;
+    padding: 5px;
+    text-align: center;
+  }
+
+  #modification{
+    text-align: end;
+    float: top;
+    position: relative;
+    margin-bottom: 10px;
+
+  }
+
+  #titre{
+    text-align: start;
+  }
+  
+  button{
+    border-radius: 5px;
+    border-color: transparent;
+    padding: 10px;
+  }
+
+  #ajout:hover{
+    background-color: lightskyblue;
+  }
+  #suppression:hover{
+    background-color: lightcoral;
+  }
+
+
+td{
+  padding: 2px;
+  border-top: 1px solid black;
+}
+</style>
 <?php
 
 // URL de l'API GraphQL
-$url = 'http://localhost:5001/graphql';
+$url = 'http://localhost:5002/graphql';
 
 // Requête GraphQL pour récupérer la liste des films
-$query =
-'{
-  films {
-    id
-    titre
-    duree
-    Realisateur
-    synopsis
-  }
-}';
+$query ='{films {
+  id
+  titre
+  duree
+  Realisateur
+  synopsis
+}}';
 
 // Envoi de la requête GraphQL à l'API
 $ch = curl_init();
@@ -29,11 +68,50 @@ curl_close($ch);
 $data = json_decode($response, true);
 $films = $data['data']['films'];
 
-// Affichage des films
-foreach ($films as $film) {
-    echo '<h2>' . $film['titre'] . '</h2>';
-    echo '<p><strong>Réalisateur :</strong> ' . $film['Realisateur'] . '</p>';
-    echo '<p><strong>Durée :</strong> ' . $film['duree'] . ' minutes</p>';
-    echo '<p><strong>Synopsis :</strong> ' . $film['synopsis'] . '</p>';
-    echo '<hr>';
-}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>IMDB</title>
+</head>
+<body>
+
+<div class="ui container">
+    <h1>Liste des films</h1>
+    <table class="ui celled table">
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>Réalisateur</th>
+                <th>Durée</th>
+                <th>Synopsis</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($films as $film) { ?>
+                <tr>
+                    <td><?php echo $film['titre']; ?></td>
+                    <td><?php echo $film['Realisateur']; ?></td>
+                    <td><?php echo $film['duree'] . ' min'; ?></td>
+                    <td><?php echo $film['synopsis']; ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
+<div style="margin-top: 20px; float:inline-end">
+        
+        <button id="ajout" onclick="">Ajouter un film</button>
+
+        <button id="suppression" onclick="">supprimer un film</button>
+        
+      </div>
+
+</body>
+
+</html>
+
