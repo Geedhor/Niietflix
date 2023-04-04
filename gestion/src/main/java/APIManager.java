45d80c2bae0f6ee970a7043e29ede9f14f9c5bee
Reyores <royeralexandre@outlook.fr>
2023-04-04 22:50:13 +0200

    Update final gestion

---
+++ gestion/src/main/java/APIManager.java
@@ -0,0 +1,61 @@
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class APIManager {
+
+    APIGestion apig;
+    GraphQLClient gc;
+
+    public APIManager(){
+        apig = new APIGestion();
+        gc = new GraphQLClient();
+    }
+
+
+    public ArrayList<String> getLsFilm(){
+        ArrayList<String> res = new ArrayList<>();
+        JSONArray lf = gc.getListFilms();
+        for(int i = 0; i < lf.length(); i++){
+        JSONObject jso = lf.getJSONObject(i);
+
+        res.add(jso.getString("titre") + "-"  +jso.getString("id"));
+
+        }
+        return res;
+    }
+
+
+
+    public void ajoutBDDSeance(String date,String heure,String film_id,String salle){
+        try {
+            apig.createSeance(date,heure,film_id,salle);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public ArrayList<String> getAllSeances() throws IOException, InterruptedException {
+        return apig.getAllSeances();
+    }
+
+
+    public void supprimerBDDSeance(int id){
+        try {
+            apig.deleteSeance(id);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
